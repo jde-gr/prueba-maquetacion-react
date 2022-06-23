2cc721386f796434e79307ca9241704bb1f46f71
jde-gr <fjavier.delgado.esteban@gmail.com>
2022-06-23 13:40:40 +0200

    Step25-Added bootstrap carousel to simple and agile module

--- src/ModuleSimpleAndAgile/components/SAImageComponent/SAImageComponent.tsx
+++ src/ModuleSimpleAndAgile/components/SAImageComponent/SAImageComponent.tsx
@@ -1,5 +1,18 @@
 import React, { FC, useState, useEffect } from 'react';
+import { Carousel } from 'react-bootstrap';
 
+import styles from './SAImageComponent.module.css';
+import img1 from '../../../components/images/SimpleAndAgileSlider/md/slides/simple_and_agile_slider_md_1.png';
+import img2 from '../../../components/images/SimpleAndAgileSlider/md/slides/simple_and_agile_slider_md_2.png';
+import img3 from '../../../components/images/SimpleAndAgileSlider/md/slides/simple_and_agile_slider_md_3.png';
+import img4 from '../../../components/images/SimpleAndAgileSlider/md/slides/simple_and_agile_slider_md_4.png';
+import img5 from '../../../components/images/SimpleAndAgileSlider/md/slides/simple_and_agile_slider_md_5.png';
+
+import img1xs from '../../../components/images/SimpleAndAgileSlider/xs/slides/simple_and_agile_slider_xs_1.png';
+import img2xs from '../../../components/images/SimpleAndAgileSlider/xs/slides/simple_and_agile_slider_xs_2.png';
+import img3xs from '../../../components/images/SimpleAndAgileSlider/xs/slides/simple_and_agile_slider_xs_3.png';
+import img4xs from '../../../components/images/SimpleAndAgileSlider/xs/slides/simple_and_agile_slider_xs_4.png';
+import img5xs from '../../../components/images/SimpleAndAgileSlider/xs/slides/simple_and_agile_slider_xs_5.png';
 interface SAImageComponentProps {
   imgWidth: number;
   imgHeight: number;
@@ -17,17 +30,69 @@ const SAImageComponent: FC<SAImageComponentProps> = (props) => {
   }, []);
 
   return (
-    <img
-      src={
+    <div
+      className={styles.simpleAndAgileCarousel}
+      style={
         windowWidth < breakPoint
-          ? '/images/Slider@3x.png'
-          : '/images/Slider_VER2.svg'
+          ? { marginBottom: 32 }
+          : { width: props.imgWidth }
       }
-      alt="Tecnología Inteligente"
-      width={props.imgWidth}
-      height={props.imgHeight}
-      style={windowWidth < breakPoint ? { marginBottom: 32 } : {}}
-    />
+    >
+      <Carousel controls={false}>
+        <Carousel.Item>
+          <img
+            src={windowWidth < breakPoint ? img1xs : img1}
+            alt="First slide"
+            width={props.imgWidth}
+            height={props.imgHeight}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src={windowWidth < breakPoint ? img2xs : img2}
+            alt="Second slide"
+            width={props.imgWidth}
+            height={props.imgHeight}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src={windowWidth < breakPoint ? img3xs : img3}
+            alt="Third slide"
+            width={props.imgWidth}
+            height={props.imgHeight}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src={windowWidth < breakPoint ? img4xs : img4}
+            alt="Fourth slide"
+            width={props.imgWidth}
+            height={props.imgHeight}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src={windowWidth < breakPoint ? img5xs : img5}
+            alt="Fifth slide"
+            width={props.imgWidth}
+            height={props.imgHeight}
+          />
+        </Carousel.Item>
+      </Carousel>
+
+      {/* <img
+        src={
+          windowWidth < breakPoint
+            ? '/images/Slider@3x.png'
+            : '/images/Slider_VER2.svg'
+        }
+        alt="Cuéntanos tu necesidad"
+        width={props.imgWidth}
+        height={props.imgHeight}
+        style={windowWidth < breakPoint ? { marginBottom: 32 } : {}}
+      /> */}
+    </div>
   );
 };
 
